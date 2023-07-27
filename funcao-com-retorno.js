aad263f289712735ece0e0e6a835b04fb31a88d4
Taturick <noturno2drick@gmail.com>
2023-07-27 09:41:00 -0300

    Ex basico de funções com retorno

---
+++ funcao-com-retorno.js
@@ -0,0 +1,11 @@
+function verificarMaiorIdade(idade){
+   if (idade >=18){
+return true;
+}else {
+    return false;
+}
+}
+
+const valorRetornadoPelaFuncao = verificarMaiorIdade(20);
+
+console.log(valorRetornadoPelaFuncao);
