94b5308b985e0194501c79e4b71419d77d665b84
Taturick <noturno2drick@gmail.com>
2023-07-27 18:04:55 -0300

    arrow

---
+++ arrow-function.js
@@ -0,0 +1,12 @@
+// function soma(x,y){
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+//declaracao com arrow function
+const soma = (x,y) =>{
+    const resultado = x + y;
+    return resultado;
+}
+
+console.log(soma(5,2))
